d5354cef6593e0639bb0b2383f2086dec8ff0c88
Kiril Popov <kiril.popov@tick42.com>
2016-07-26 17:11:25 +0300

    initial commit

---
+++ tests/index.js
@@ -0,0 +1,60 @@
+var expect = require('chai').expect;
+nocache = function(module) {
+    delete require.cache[require.resolve(module)];
+    return require(module);
+};
+
+var Registry = nocache('../lib/index.js');
+
+describe('callback-registry', function (done) {
+
+    it ('should execute single callback', function (done) {
+        var registry = Registry();        
+        registry.add('test', done);        
+        registry.execute('test');
+    });
+
+    it ('should execute multiple callbacks', function (done) {
+        var registry = Registry();
+        var invoked = 0;
+        var p = function(){
+            invoked ++;
+            if (invoked == 3){
+                done();
+            }
+        }        
+        registry.add('test', p);        
+        registry.add('test', p);
+        registry.add('test', p);
+        registry.execute('test');
+    });
+
+    it ('should not execute callbacks on different keys', function (done) {
+        var registry = Registry();
+        var invoked = 0;
+        var p1 = function(){
+            invoked ++;
+            if (invoked == 2){
+                done();
+            }
+        }        
+        var p2 = function(){
+            done('should not be invoked');
+        }
+        registry.add('test1', p1);        
+        registry.add('test2', p2);
+        registry.execute('test1');
+        registry.execute('test1');
+    });       
+
+    it ('should be able to remove callback', function (done) {
+        var registry = Registry();        
+        var callbackKey = registry.add('test', function(){
+            console.log('!!! ERROR');
+            done('should not be executed');
+        });
+        registry.remove(callbackKey);        
+        registry.execute('test');
+        done();
+    }); 
+});
