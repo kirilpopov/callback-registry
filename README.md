727ca90ef268512914d44658af8c214e6b2e4fd0
Kiril Popov <kiril.popov@tick42.com>
2016-07-26 17:37:42 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # callback-registry
 Registry for callbacks
+
+A simple registry that allows you to add one or more callbacks, where each callback is added with some key and then execute all callbacks by just using the key.
+
+Similar to EventEmitter but not limited to Node.js.
+
+```javascript
+
+const Registry = require('../lib/index.js');
+const registry = Registry();
+
+// add a new callback for that event key
+registry.add('event-key', function(){
+    console.log('the event occurred')
+});
+
+registry.execute('event-key');
+
+```
--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-nocache = function(module) {
+nocache = function (module) {
     delete require.cache[require.resolve(module)];
     return require(module);
 };
@@ -8,53 +8,66 @@ var Registry = nocache('../lib/index.js');
 
 describe('callback-registry', function (done) {
 
-    it ('should execute single callback', function (done) {
-        var registry = Registry();        
-        registry.add('test', done);        
+    it('should execute single callback', function (done) {
+        var registry = Registry();
+        registry.add('test', done);
         registry.execute('test');
     });
 
-    it ('should execute multiple callbacks', function (done) {
+    it('should execute multiple callbacks', function (done) {
         var registry = Registry();
         var invoked = 0;
-        var p = function(){
-            invoked ++;
-            if (invoked == 3){
+        var p = function () {
+            invoked++;
+            if (invoked == 3) {
                 done();
             }
-        }        
-        registry.add('test', p);        
+        }
+        registry.add('test', p);
         registry.add('test', p);
         registry.add('test', p);
         registry.execute('test');
     });
 
-    it ('should not execute callbacks on different keys', function (done) {
+    it('should not execute callbacks on different keys', function (done) {
         var registry = Registry();
         var invoked = 0;
-        var p1 = function(){
-            invoked ++;
-            if (invoked == 2){
+        var p1 = function () {
+            invoked++;
+            if (invoked == 2) {
                 done();
             }
-        }        
-        var p2 = function(){
+        }
+        var p2 = function () {
             done('should not be invoked');
         }
-        registry.add('test1', p1);        
+        registry.add('test1', p1);
         registry.add('test2', p2);
         registry.execute('test1');
         registry.execute('test1');
-    });       
+    });
 
-    it ('should be able to remove callback', function (done) {
-        var registry = Registry();        
-        var callbackKey = registry.add('test', function(){
+    it('should be able to remove callback', function (done) {
+        var registry = Registry();
+        var callbackKey = registry.add('test', function () {
             console.log('!!! ERROR');
             done('should not be executed');
         });
-        registry.remove(callbackKey);        
+        registry.remove(callbackKey);
         registry.execute('test');
         done();
-    }); 
+    });
+
+    it('should pass arguments', function (done) {
+        var registry = Registry();
+        registry.add('test', function (a, b, c) {
+            console.log(arguments);
+            if (a === 1 && b === '2' && c === 3) {
+                done();
+            } else {
+                done('error - arguments not matching');
+            }
+        });
+        registry.execute('test', 1, '2', 3);
+    });
 });
