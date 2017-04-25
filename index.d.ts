81c3da61bc2f90cd26a519414a3cddf4a6874d80
Kiril Popov <kiril.popov@tick42.com>
2017-04-25 10:49:37 +0300

    2.2.5 updated d.ts

--- index.d.ts
+++ index.d.ts
@@ -7,14 +7,14 @@ export interface CallbackRegistry {
      * Adds a new callback to the registry under some key.
      * The callback will be notified when someone executes
      */
-    add(key: string, callback: (...args) => any): UnsubscribeFunction;
+    add(key: string, callback: (...args: any[]) => any): UnsubscribeFunction;
 
     /**
      * Executes all callbacks registered for a certain key.
      * Optional arguments can be supplied - these will reach
      * callbacks
      */
-    execute(key: string, ...argumentsArr): object[];
+    execute(key: string, ...argumentsArr: any[]): object[];
 }
 
 export default function(): CallbackRegistry;
