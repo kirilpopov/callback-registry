2a758b9a9ef82a311e5ae81d1f4b5940b312ba90
Kiril Popov <kiril.popov@tick42.com>
2017-04-25 10:43:14 +0300

    2.2.3 added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+export interface UnsubscribeFunction {
+    (): void;
+}
+
+export interface CallbackRegistry {
+    /**
+     * Adds a new callback to the registry under some key.
+     * The callback will be notified when someone executes
+     */
+    add(key: string, callback: () => any): UnsubscribeFunction;
+
+    /**
+     * Executes all callbacks registered for a certain key.
+     * Optional arguments can be supplied - these will reach
+     * callbacks
+     */
+    execute(key: string, ...argumentsArr): object[];
+}
+
+export default function(): CallbackRegistry;
